2d2d611c26c1f8fc33e4b2d8f588f18c5ade2471
xirade <6myusic6@gmail.com>
2022-01-12 14:54:27 +0200

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Logo from "./assets/logo.png"
+import Logo from "./assets/logo.png";
 import BarChart from "./components/BarChart";
 import Table from "./components/Table";
 
 export default function App() {
-  const planetsNames = new Set([
-    "Tatooine",
-    "Alderaan",
-    "Naboo",
-    "Bespin",
-    "Endor"
-  ]);
-
   const [data, setData] = useState(null);
   const [planets, setPlanets] = useState(null);
   const getJson = (url) => {
@@ -26,38 +18,49 @@ export default function App() {
       });
   };
 
-  useEffect(async () => {
-    const resultVehicles = await getJson(
-      "https://swapi.py4e.com/api/vehicles"
-    ).then(
-      async (json) =>
-        await Promise.all(
-          json.results
-            .filter((vehicle) => vehicle.pilots.length)
-            .map(async (vehicle) => ({
-              name: vehicle.name,
-              pilot: await Promise.all(
-                vehicle.pilots.map((pilot) =>
-                  getJson(pilot).then(async (json) => ({
-                    name: json.name,
-                    planet: await getJson(json.homeworld).then((json) => ({
+  useEffect(() => {
+    const planetsNames = new Set([
+      "Tatooine",
+      "Alderaan",
+      "Naboo",
+      "Bespin",
+      "Endor"
+    ]);
+    const fetchAll = async () => {
+      const resultVehicles = await getJson(
+        "https://swapi.py4e.com/api/vehicles"
+      ).then(
+        async (json) =>
+          await Promise.all(
+            json.results
+              .filter((vehicle) => vehicle.pilots.length)
+              .map(async (vehicle) => ({
+                name: vehicle.name,
+                pilot: await Promise.all(
+                  vehicle.pilots.map((pilot) =>
+                    getJson(pilot).then(async (json) => ({
                       name: json.name,
-                      population: json.population
+                      planet: await getJson(json.homeworld).then((json) => ({
+                        name: json.name,
+                        population: json.population
+                      }))
                     }))
-                  }))
+                  )
                 )
-              )
-            }))
-        )
-    );
+              }))
+          )
+      );
 
-    const resultPlanets = await getJson(
-      "https://swapi.py4e.com/api/planets"
-    ).then((json) =>
-      json.results.filter((result) => planetsNames.has(result.name))
-    );
-    setData(resultVehicles);
-    setPlanets(resultPlanets);
+      const resultPlanets = await getJson(
+        "https://swapi.py4e.com/api/planets"
+      ).then((json) =>
+        json.results.filter((result) => planetsNames.has(result.name))
+      );
+      setData(resultVehicles);
+      setPlanets(resultPlanets);
+    };
+
+    fetchAll();
   }, []);
 
   const vehicleWithTotal =
